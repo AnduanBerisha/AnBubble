896e7c84e4dd0c9ee33c3082f60b4899b565556e
AnduanBerisha <183530204+AnduanBerisha@users.noreply.github.com>
2026-08-08 23:11:11 +0200

    feat: assembling http routes in the main file

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
+// Init server & attaching everything
 func main() {
+	server := NewServer()
+
 	http.Handle("/", http.FileServer(http.Dir("web")))
+
+	// Attaching route
+	http.HandleFunc("/send", server.handleSend)
+
+	// Attaching msgs
+	http.HandleFunc("/messages", server.handleMessages)
+
+	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
