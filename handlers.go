a7bbfb8b8695b67a1b27127101c4ca6fc346711d
AnduanBerisha <183530204+AnduanBerisha@users.noreply.github.com>
2026-08-08 22:58:27 +0200

    feat: add handleMessages endpoint

--- handlers.go
+++ handlers.go
@@ -27,3 +27,18 @@ func (s *Server) handleSend(w http.ResponseWriter, r *http.Request) {
 	// All good code 200
 	w.WriteHeader(http.StatusOK)
 }
+
+func (s *Server) handleMessages(w http.ResponseWriter, r *http.Request) {
+
+	// Only allow GET
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	messages := s.GetMessages()
+
+	json.NewEncoder(w).Encode(messages)
+}
